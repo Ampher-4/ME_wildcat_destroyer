a708757138e6db1bbb7e664b04b62812a459c178
pisentry highcmd <invalid@no.com>
2025-12-20 21:04:12 +0800

    switch to pigpio

--- sentry.py
+++ sentry.py
@@ -5,7 +5,7 @@ import os
 import time
 import cv2
 from ultralytics.models.yolo import YOLO
-import RPi.GPIO as GPIO
+import pigpio
 import threading
 
 
@@ -32,18 +32,19 @@ def camera_thread(cap):
 SERVO_PIN = 18       # PWM 引脚
 LED_PIN = 23         # 爆闪 LED
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-GPIO.setup(SERVO_PIN, GPIO.OUT)
-servo = GPIO.PWM(SERVO_PIN, 50)  # SG90: 50Hz
-servo.start(0)
 
 # SG90 工具函数：角度→占空比
 def set_servo_angle(angle):
     angle = max(0, min(180, angle))
-    duty = 2 + (angle / 18)
-    servo.ChangeDutyCycle(duty)
+
+    # SG90 常见参数（可微调）
+    min_pulse = 500    # μs
+    max_pulse = 2500   # μs
+
+    pulse_width = min_pulse + (angle / 180.0) * (max_pulse - min_pulse)
+
+    pi.set_servo_pulsewidth(SERVO_PIN, pulse_width)
+    
 
 
 # =========================
@@ -51,18 +52,18 @@ def set_servo_angle(angle):
 # =========================
 def flash_led(times=3, interval=0.1):
     for _ in range(times):
-        GPIO.output(LED_PIN, True)
+        pi.write(LED_PIN, 1)
         time.sleep(interval)
-        GPIO.output(LED_PIN, False)
+        pi.write(LED_PIN, 0)
         time.sleep(interval)
 
 
 # =========================
 # 扫描模式参数
 # =========================
-scan_angle = 0
+scan_angle = 90
 scan_direction = 1  # 1:右走, -1:左走
-SCAN_SPEED = 1       # 每帧移动多少°
+SCAN_SPEED = 2       # 每帧移动多少°
 SCAN_MIN = 20        # 最左扫描角度
 SCAN_MAX = 160       # 最右扫描角度
 
@@ -83,7 +84,7 @@ def run_sentry(camera_index=0, operatingmode=0, cv2cap=None):
     # 加载 YOLO
     model = YOLO("yolov8n_ncnn_model")
 
-    cv2.namedWindow("Sentry Cat Turret", cv2.WINDOW_NORMAL)
+#    cv2.namedWindow("Sentry Cat Turret", cv2.WINDOW_NORMAL)
 
 
     print("哨戒猫炮塔启动！按 q 或 Ctrl+C 退出...")
@@ -112,38 +113,38 @@ def run_sentry(camera_index=0, operatingmode=0, cv2cap=None):
                 conf = float(box.conf[0])
 
                 # cv2 debug
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(
-                    frame,
-                    f"cat {conf:.2f}",
-                    (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.6,
-                    (0, 255, 0),
-                    2
-                )
+#                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+#                cv2.putText(
+#                    frame,
+#                    f"cat {conf:.2f}",
+#                    (x1, y1 - 10),
+#                    cv2.FONT_HERSHEY_SIMPLEX,
+#                    0.6,
+#                    (0, 255, 0),
+#                    2
+#                )
 
 
                 # 计算猫中心
                 cat_x = (x1 + x2) // 2
                 frame_center = frame.shape[1] // 2
 
-                #cv2 debug
-                cv2.line(frame,
-                         (frame_center, 0),
-                         (frame_center, frame.shape[0]),
-                         (255, 0, 0), 1)
+#                #cv2 debug
+#                cv2.line(frame,
+#                         (frame_center, 0),
+#                         (frame_center, frame.shape[0]),
+#                         (255, 0, 0), 1)
 
                 # 偏差（像素差）
                 error_x = cat_x - frame_center
-                correction = error_x * 0.05  # 需要你现场调参
+                correction = error_x * 0.1  # adjust this !!!
 
                 # 更新云台角度
                 scan_angle -= correction
                 scan_angle = max(SCAN_MIN, min(SCAN_MAX, scan_angle))
                 set_servo_angle(scan_angle)
 
-                flash_led(times=1, interval=0.05)
+                flash_led(interval=0.05)
                 detected = True
 
                 print(f"锁定猫 angle={scan_angle:.2f}° error={error_x}")
@@ -159,8 +160,6 @@ def run_sentry(camera_index=0, operatingmode=0, cv2cap=None):
                 set_servo_angle(scan_angle)
 
 
-            cv2.imshow("Sentry Cat Turret", frame)
-            # 按 q 退出
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -170,14 +169,19 @@ def run_sentry(camera_index=0, operatingmode=0, cv2cap=None):
         print("\n手动中断")
 
     finally:
-        servo.stop()
-        GPIO.cleanup()
         cap.release()
-        cv2.destroyAllWindows()
+        set_servo_angle(90)
 
 
 
 if __name__ == "__main__":
+    print("gpio debug")
+    pi = pigpio.pi()
+    print(pi.connected)
+    pi.set_mode(SERVO_PIN, pigpio.OUTPUT)
+    pi.set_mode(LED_PIN, pigpio.OUTPUT)
+    pi.write(LED_PIN, 0)
+
     cam = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 
     #init the system
